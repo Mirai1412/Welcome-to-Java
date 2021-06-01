dea789597964e2b26cd5a08c92a83767ff876622
mirai1412 <kkas0807@naver.com>
2021-06-01 18:10:58 +0900

    LeapYear

---
+++ src/LeapYear.java
@@ -0,0 +1,14 @@
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int year = 2012;
+		boolean isLeapYear;	//À±³â °è»ê
+		
+		isLeapYear = (year % 4 == 0);
+		System.out.println(isLeapYear);
+	}
+
+}
