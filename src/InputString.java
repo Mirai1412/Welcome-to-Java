5f9ce7283d15cdacb29fb244dbc4f176e28924cf
mirai1412 <kkas0807@naver.com>
2021-06-02 16:40:02 +0900

    문자열 숫자열 출력

---
+++ src/InputString.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class InputString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name;
+		int age;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("이름을 입력하시요 :");
+		name = sc.nextLine();
+		
+		System.out.println("나이를 입력하시요 :");
+		age = sc.nextInt();
+		
+		System.out.println(name + "님 안녕하세요! " + (age) + "살이시네요.");
+	}
+
+}
